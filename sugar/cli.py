361832d686c4c9a94aed3b2bd9f52d16fbf66ea5
Bo Maryniuk <bo@suse.de>
2018-12-05 18:12:06 +0100

    Refactor structure

---
+++ sugar/cli.py
@@ -0,0 +1,73 @@
+"""
+Main app file, processing CLI
+"""
+
+import argparse
+import sys
+
+
+class SugarCLI(object):
+    """
+    CLI for running Sugar components in Git style command line interface.
+    """
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Sugar allows for commands to be executed across a space of remote systems in parallel, "
+                        "so they can be both controlled and queried with ease.",
+            usage="""sugar [<target>] [<component>] [<args>]
+
+Target is a name or a pattern in Unix shell-style
+wildcard that matches client names.
+
+Available components:
+
+    master     Sugar Master, used to control Sugar Clients
+    client     Sugar Client, receives commands from a remote Sugar Master
+    local      Sugar local orchestration""")
+        parser.add_argument('component', help='Component to run')
+        args = parser.parse_args(sys.argv[1:2])
+        if SugarCLI.is_target(args.component):
+            print('Not implemented yet')
+            sys.exit(1)
+
+        if not hasattr(self, args.component):
+            print('Unrecognized command')
+            parser.print_help()
+            sys.exit(1)
+
+        getattr(self, args.component)()
+
+    @staticmethod
+    def is_target(command):
+        """
+        Checks if the command is a target.
+
+        :param command:
+        :return:
+        """
+        # See if it matches any of registered clients
+        return "*" in command  # TODO: hack-stub
+
+    def master(self):
+        """
+        Sugar Master starter.
+        :return:
+        """
+        parser = argparse.ArgumentParser(
+            description='Sugar Master, used to control Sugar Clients')
+        parser.add_argument('-l', '--log-level', help='Log level',
+                            choices=['info', 'error', 'warning', 'debug'])
+        args = parser.parse_args(sys.argv[2:])
+        print('Log: %s' % args.log)
+
+    def client(self):
+        """
+        Sugar Client starter.
+        :return:
+        """
+
+    def local(self):
+        """
+        Sugar local caller (orchestration)
+        :return:
+        """
