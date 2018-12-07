f2747d9550ec2fa4ec37b8bd10e36c7ec96322be
Bo Maryniuk <bo@suse.de>
2018-12-07 10:15:38 +0100

    Implement Twisted's logging wrapper

---
+++ sugar/lib/logger.py
@@ -0,0 +1,123 @@
+# coding: utf-8
+"""
+Logger extensions for Twisted.
+"""
+import sys
+import logging
+
+from twisted.python import log, util
+from twisted.python.logfile import LogFile
+
+from sugar.utils.objects import Singleton
+from sugar.lib import six
+from sugar.config import get_config
+
+
+class SugarLogObserver(log.FileLogObserver):
+    """
+    Logging messages observer.
+    """
+    def __init__(self, fh, level=logging.INFO):
+        log.FileLogObserver.__init__(self, fh)
+        self.log_level = level
+
+    def __call__(self, event_data):
+        self.emit(event_data)
+
+    def _get_log_level(self, event_data):
+        """
+        Get proper logging level
+        :param level:
+        :return:
+        """
+        level = logging.ERROR if bool(event_data.get('isError')) else event_data.get('level', logging.INFO)
+        return self.log_level if level < self.log_level else level
+
+    def emit(self, event_data):
+        """
+        Log message emitter
+        """
+
+        msg = log.textFromEventDict(event_data)
+        if msg:
+            if six.PY3:
+                if isinstance(msg, six.binary_type):
+                    msg = msg.decode("utf-8")
+            else:
+                if isinstance(msg, six.text_type):
+                    msg = msg.encode("utf-8")
+
+            msg = "{tm} {lvl}:[{ssm}]: {txt}\n".format(tm=self.formatTime(event_data['time']),
+                                                       lvl=logging.getLevelName(self._get_log_level(event_data)),
+                                                       ssm=event_data['system'], txt=msg)
+            util.untilConcludes(self.write, msg)
+            util.untilConcludes(self.flush)
+
+
+class Logger(object):
+    """
+    Turns log levels into `twisted.python.log.msg` corresponding method alias.
+    Result is before:
+
+      log.msg(message, level=logging.INFO, system='hello')
+
+    After:
+
+      log.info(message)
+
+    """
+    LOG_LEVELS = {
+        'all': logging.NOTSET,
+        'debug': logging.DEBUG,
+        'error': logging.ERROR,
+        'critical': logging.CRITICAL,
+        'info': logging.INFO,
+        'warning': logging.WARNING,
+        'fatal': logging.FATAL
+    }
+
+    def __init__(self, name):
+        self.name = name
+        for method in self.LOG_LEVELS:
+            def make_log_level_caller(level):
+                """
+                Wrap Twisted's log into levels
+                :param level:
+                :return:
+                """
+                def _msg(message):
+                    log.msg(message, level=level, system=self.name)
+                return _msg
+
+            setattr(self, method, make_log_level_caller(self.LOG_LEVELS[method]))
+
+
+@Singleton
+class LoggerManager(object):
+    """
+    Logger manager.
+    """
+
+    # TODO: get all that from the config (and the whole below)
+    def __init__(self):
+        self.config = get_config()
+        self.logger_store = {}
+
+        for log_cfg in self.config.log:
+            path = log_cfg.file if log_cfg.file not in ['STDOUT', 'STDERR'] else None
+            device = LogFile.fromFullPath(
+                path, rotateLength=0xa00000, maxRotatedFiles=10) if path else getattr(sys, log_cfg.file.lower())
+            log.addObserver(SugarLogObserver(device, Logger.LOG_LEVELS[log_cfg.level]))
+
+    def get_logger(self, name):
+        """
+        Get logger with the specified name
+        """
+        if not isinstance(name, six.string_types):
+            # TODO: Have sugar exceptions!
+            raise Exception("Logger name should be a string type")
+
+        return self.logger_store.setdefault(name, Logger(name))
+
+
+get_logger = LoggerManager().get_logger
