a017b8d023004efc2823287e83bb58ef5f90f1ad
Bo Maryniuk <bo@suse.de>
2018-12-11 14:56:43 +0100

    Attempt to parse target query

--- sugar/console/__init__.py
+++ sugar/console/__init__.py
@@ -6,12 +6,15 @@ Sugar Server
 
 from __future__ import unicode_literals, print_function, absolute_import
 
+import sys
 from twisted.internet import reactor, ssl
 
 from autobahn.twisted.websocket import connectWS
 from sugar.console.protocols import SugarConsoleProtocol, SugarClientFactory
 from sugar.config import get_config
 from sugar.lib.logger.manager import get_logger
+from sugar.transport import ConsoleMsgFactory
+from sugar.lib import exceptions
 
 log = get_logger(__name__)
 
@@ -24,12 +27,71 @@ __email__ = "bo@maryniuk.net"
 __status__ = "Damn Bloody Alpha"
 
 
+class SugarConsoleCore(object):
+    """
+    Sugar console core class.
+    """
+
+    def __init__(self, args):
+        """
+        Constructor.
+
+        :param args:
+        """
+        self.args = args
+
+    def _get_type(self, val):
+        """
+        Determine type of the value.
+
+        :param val:
+        :return:
+        """
+        return val
+
+    def _get_args(self, query):
+        """
+        Get args from the query
+
+        :param query:
+        :return:
+        """
+        args = []
+        kwargs = {}
+        for arg in query:
+            if "=" not in arg:
+                args.append(arg)
+            else:
+                k, v = arg.split('=', 1)
+                kwargs[k] = self._get_type(v)
+        return args, kwargs
+
+    def parse_command_line(self):
+        """
+        Parse command line command.
+        This finds target, function and parameters.
+
+        :return:
+        """
+        target = sys.argv[1:2]
+        query = self.args.query[::]
+        print(">>>", query)
+        print('>>>', target)
+
+        cnt = ConsoleMsgFactory.create()
+        cnt.tgt = target
+        cnt.fun = query.pop()
+        cnt.arg = self._get_args(query)
+
+        return cnt
+
+
 class SugarConsole(object):
     """
     Sugar console class.
     """
 
-    def __init__(self):
+    def __init__(self, args):
         """
         Init
         :param url:
@@ -40,6 +102,7 @@ class SugarConsole(object):
 
         log.debug('Socket ')
         self.factory = SugarClientFactory(url)
+        self.factory.console = SugarConsoleCore(args)
         if not self.factory.isSecure:
             raise Exception('Unable to initialte TLS')
 
