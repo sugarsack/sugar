f1d139ab9585d288f4779fed0022c7f3d2cc3261
Bo Maryniuk <bo@suse.de>
2018-12-16 19:53:21 +0100

    Rewrite keystore for SQLite3 with PonyORM support

--- sugar/lib/pki/keystore.py
+++ sugar/lib/pki/keystore.py
@@ -1,130 +1,55 @@
 """
-Keystore and clients db.
+Testing peewee
 """
-from __future__ import absolute_import, print_function, unicode_literals
+
+from __future__ import absolute_import, unicode_literals, print_function
 
 import os
 import time
-import pickle
 
 from sugar.lib.pki import Crypto
+from sugar.lib import six
+from sugar.utils.cli import get_current_component
 import sugar.utils.files
 import sugar.utils.stringutils
 import sugar.lib.exceptions
-from sugar.lib import six
-from sugar.cli import SugarCLI
+
+from pony import orm
 
 
-class StoredKey(object):
+class KeyStore(object):
     """
-    Machine public key with the metadata.
-    It is assigned to the specific unique ID of the machine.
+    Key Store implementation with the SQLite3.
     """
+    db = orm.Database()
+
     STATUS_CANDIDATE = "candidate"
     STATUS_ACCEPTED = "accepted"
     STATUS_DENIED = "denied"
     STATUS_REJECTED = "rejected"
 
-    def __init__(self, name, pubkey_pem):
-        """
-        Key container object.
-
-        :param pubkey_pem: Public key (PEM)
-        """
-        self.__pem = pubkey_pem
-        self.__name = name
-        self.__fingerprint = Crypto.get_finterprint(self.__pem)
-        self.__status = self.STATUS_CANDIDATE
-
-    @property
-    def name(self):
-        """
-        Return key name (simplified fingerprint).
-
-        :return:
-        """
-        return self.__name
-
-    @property
-    def fingerprint(self):
-        """
-        Return fingerprint of the machine.
-        :return:
-        """
-        return self.__fingerprint
-
-    def accept(self):
-        """
-        Accept key.
-
-        :return:
-        """
-        self.__status = self.STATUS_ACCEPTED
-
-    def reject(self):
-        """
-        Reject key.
-
-        :return:
-        """
-        self.__status = self.STATUS_REJECTED
-
-    def deny(self):
-        """
-        Deny key.
-
-        :return:
-        """
-        self.__status = self.STATUS_DENIED
-
-
-class _KeyStoreDB(object):
-    """
-    KeyStore database class.
-    """
     LOCKFILE = ".keystore.lck"
+    DBFILE = "keystore.db"
 
-    def __init__(self, path, component=None):
+    def __init__(self, path):
         """
-        Keystore main class.
+        Constructor.
 
-        :param path: Path to the keystore.
+        :param dbpath:
         """
-        if component is None or component not in SugarCLI.COMPONENTS:
-            raise sugar.lib.exceptions.SugarRuntimeException("Unknown component")
-        self.__component = component
+        self.__component = get_current_component()
 
         if self.__component != 'local':
-            self.__root_path = os.path.join(path, component)
+            self.__root_path = os.path.join(path, self.__component)
             self.__keys_path = sugar.utils.files.mk_dirs(os.path.join(self.__root_path, 'keys'))
-            self.__space_path = sugar.utils.files.mk_dirs(os.path.join(self.__root_path, 'space'))
         self.__lock_file_path = os.path.join(self.__root_path, self.LOCKFILE)
         self.__is_locked = False
 
-        # Indexes
-        self.__index_fingpt = {}
-        self.__index_candidate = {}
-        self.__index_accepted = {}
-        self.__index_rejected = {}
-        self.__index_denied = {}
-
-    def _store_key(self, key):
-        """
-        Store key in the FS, add to the index.
-
-        :param key:
-        :return:
-        """
-
-        # with sugar.utils.files.atomic_write()
-
-    def _remove_key(self, key):
-        """
-        Remove key from the FS, delete from the index.
-
-        :param key:
-        :return:
-        """
+        db_name = os.path.join(self.__root_path, self.DBFILE)
+        db_mapping = not os.path.exists(db_name)
+        self.db.bind(provider="sqlite", filename=db_name, create_db=True)
+        if db_mapping:
+            self.db.generate_mapping(create_tables=True)
 
     def _lock_transation(self, timeout=30):
         """
@@ -141,7 +66,7 @@ class _KeyStoreDB(object):
             else:
                 break
 
-        with sugar.utils.files.fopen(self.__lock_file_path, 'w') as fh_lck:
+        with sugar.utils.files.fopen(self.__lock_file_path, 'wb') as fh_lck:
             fh_lck.write(sugar.utils.stringutils.to_bytes(six.text_type(os.getpid())))
             self.__is_locked = True
 
@@ -175,18 +100,13 @@ class _KeyStoreDB(object):
 
         return not self.__is_locked
 
-
-class KeyStore(_KeyStoreDB):
-    """
-    Keystore
-    """
     def get_candidates(self):
         """
         List candidate keys in the store by name.
 
         :return:
         """
-        return self.__index_candidate.keys()
+        return orm.select(k for k in StoredKey if k.status == self.STATUS_CANDIDATE)
 
     def get_accepted(self):
         """
@@ -194,7 +114,7 @@ class KeyStore(_KeyStoreDB):
 
         :return:
         """
-        return self.__index_accepted.keys()
+        return orm.select(k for k in StoredKey if k.status == self.STATUS_ACCEPTED)
 
     def get_rejected(self):
         """
@@ -202,7 +122,7 @@ class KeyStore(_KeyStoreDB):
 
         :return:
         """
-        return self.__index_rejected.keys()
+        return orm.select(k for k in StoredKey if k.status == self.STATUS_REJECTED)
 
     def get_denied(self):
         """
@@ -210,24 +130,104 @@ class KeyStore(_KeyStoreDB):
 
         :return:
         """
-        return self.__index_denied.keys()
+        return orm.select(k for k in StoredKey if k.status == self.STATUS_DENIED)
 
-    def add(self, pubkey_pem):
+    @orm.db_session
+    def add(self, pubkey_pem, hostname, machine_id):
         """
         Add PEM key to the store.
 
         :param pubkey_pem:
         :return:
         """
-        key = StoredKey(pubkey_pem)
+        self._lock_transation()
+        key = StoredKey(hostname=hostname, fingerprint=Crypto.get_finterprint(pubkey_pem),
+                        machine_id=machine_id, filename="{}.bin".format(os.path.join(self.__keys_path, machine_id)),
+                        status=self.STATUS_CANDIDATE)
 
-    def delete(self, pubkey_pem):
+        print('HOSTNAME:', key.hostname)
+        print('MACHINE ID:', key.machine_id)
+
+        orm.commit()
+        self._unlock_transaction()
+
+    @orm.db_session
+    def delete(self, fingerprint):
         """
         Delete PEM key from the store.
 
         :param pubkey_pem:
         :return:
         """
+        self._lock_transation()
+        key = self.__get_key_for_status(fingerprint)
+        if key is not None:
+            orm.delete(k for k in StoredKey if k.fingerprint == fingerprint)
+            # delete key from the fs too
+        self._unlock_transaction()
+
+    def __get_key_for_status(self, hostname=None, fingerprint=None):
+        """
+        Get key for the status update.
+
+        :param hostname:
+        :param fingerprint:
+        :return:
+        """
+        if hostname is not None:
+            key = self.get_key_by_hostname(hostname)
+        elif fingerprint is not None:
+            key = self.get_key_by_fingerprint(fingerprint)
+        else:
+            raise sugar.lib.exceptions.SugarKeyStoreException("Hostname or fingerprint needs to be specified")
+        return key
+
+    @orm.db_session
+    def reject(self, hostname=None, fingerprint=None):
+        """
+        Reject key by either hostname or fingerprint.
+        If both are None, an exception is raised.
+
+        :param hostname:
+        :param fingerprint:
+        :return:
+        """
+        self._lock_transation()
+        key = self.__get_key_for_status(hostname=hostname, fingerprint=fingerprint)
+        if key is not None:
+            key.status = self.STATUS_REJECTED
+            orm.commit()
+        self._unlock_transaction()
+
+    @orm.db_session
+    def deny(self, fingerprint):
+        """
+        Deny key by fingerprint.
+
+        :param fingerprint:
+        :return:
+        """
+        self._lock_transation()
+        key = self.__get_key_for_status(fingerprint=fingerprint)
+        if key is not None:
+            key.status = self.STATUS_DENIED
+            orm.commit()
+        self._unlock_transaction()
+
+    @orm.db_session
+    def accept(self, fingerprint):
+        """
+        Accept key by fingerprint.
+
+        :param fingerprint:
+        :return:
+        """
+        self._lock_transation()
+        key = self.__get_key_for_status(fingerprint=fingerprint)
+        if key is not None:
+            key.status = self.STATUS_ACCEPTED
+            orm.commit()
+        self._unlock_transaction()
 
     def get_key_by_fingerprint(self, fingerprint):
         """
@@ -236,12 +236,42 @@ class KeyStore(_KeyStoreDB):
         :param fingerprint:
         :return:
         """
+        return orm.select(k for k in StoredKey if k.fingerprint == fingerprint)
 
-    def get_key_by_name(self, name):
+    def get_key_by_machine_id(self, machine_id):
         """
         Get key by name.
 
         :param name:
         :return: Usability status (boolean), key
         """
+        return orm.select(k for k in StoredKey if k.machine_id == machine_id)
 
+    def get_key_by_hostname(self, hostname):
+        """
+        Get key by hostname.
+
+        :param hostname:
+        :return:
+        """
+        return orm.select(k for k in StoredKey if k.hostname == hostname)
+
+
+class StoredKey(KeyStore.db.Entity):
+    """
+    Key meta.
+    """
+    id = orm.PrimaryKey(int, auto=True)
+    hostname = orm.Required(str, unique=True)
+    machine_id = orm.Required(str, unique=True)
+    fingerprint = orm.Required(str, unique=True)
+    status = orm.Required(str)
+    filename = orm.Required(str, unique=True)
+    notes = orm.Optional(str)
+
+
+if __name__ == '__main__':
+    ks = KeyStore("/tmp/ks")
+    c = Crypto()
+    pri, pub = c.create_rsa_keypair()
+    ks.add(pub, "bla", "blabla")
