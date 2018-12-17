f1dfa2a1713f82cda404029443a97caab689c3d5
Bo Maryniuk <bo@suse.de>
2018-12-17 21:31:30 +0100

    Implement display for the key lister

--- sugar/components/keymanager/__init__.py
+++ sugar/components/keymanager/__init__.py
@@ -4,9 +4,13 @@ Key manager component (CLI)
 
 from __future__ import unicode_literals, absolute_import, print_function
 
+import os
+import sys
+from collections import OrderedDict
 from sugar.config import get_config
 from sugar.lib.logger.manager import get_logger
 from sugar.lib.pki.keystore import KeyStore
+from sugar.lib.outputters.console import IterableOutput, TitleOutput
 
 
 class SugarKeyManager(object):
@@ -14,6 +18,7 @@ class SugarKeyManager(object):
     Sugar key manager CLI program.
     """
     OPERATION = ["accept", "deny", "reject", "list"]
+
     def __init__(self, args):
         """
         Init
@@ -22,9 +27,13 @@ class SugarKeyManager(object):
         self.config = get_config()
         self.args = args
         self.log = get_logger(self)
-        self.__keystore = KeyStore(self.config.keystore)
+        self.__keystore = KeyStore(os.path.abspath(self.args.config_dir))
 
-    def list_keys(self, status="all"):
+        self._list_output = IterableOutput(colors=self.config.terminal.colors,
+                                           encoding=self.config.terminal.encoding)
+        self._list_output._symbols_utf["n/a"] = self._list_output._symbols_ascii["n/a"] = "   No keys"
+
+    def list(self):
         """
         List keys by status.
         Possible statuses:
@@ -32,7 +41,22 @@ class SugarKeyManager(object):
 
         :return:
         """
+        title = TitleOutput(colors=self.config.terminal.colors, encoding=self.config.terminal.encoding)
+        all = [("accepted", "success"), ("rejected", "alert"), ("denied", "warning"), ("new", "info")]
+        ret = OrderedDict()
 
+        for section in all:
+            text, style = section
+            if self.args.status == "all" or self.args.status == text:
+                ret[text] = getattr(self.__keystore, "get_{}".format(text))()
+                title.add(text.title(), style)
+
+        # Short version
+        for text in ret:
+            sys.stdout.write(title.paint(text.title()) + "\n")
+            sys.stdout.write(self._list_output.paint(ret[text]) + "\n\n")
+
+        # Full version
 
 
     def run(self):
@@ -41,5 +65,5 @@ class SugarKeyManager(object):
 
         :return:
         """
-        self.log.debug(self.args)
-        print('blet')
+        self.log.debug("Running Key Manager")
+        getattr(self, self.args.command)()
