62b4551507653f9aa74a1c06a99819f83a1fcb02
Bo Maryniuk <bo@suse.de>
2018-12-22 12:26:16 +0100

    Move common operations to the utils. Rename events class

--- sugar/components/client/core.py
+++ sugar/components/client/core.py
@@ -5,15 +5,16 @@ Core client operations.
 from __future__ import unicode_literals, absolute_import
 
 import os
-import errno
 import queue
 
 from sugar.config import get_config
 from sugar.lib.logger.manager import get_logger
 from sugar.lib.pki import Crypto
+import sugar.lib.pki.utils
 from sugar.utils.objects import Singleton
 from sugar.utils.cli import get_current_component
 from sugar.transport.serialisable import Serialisable
+from sugar.transport import ClientMsgFactory, ObjectGate
 
 
 @Singleton
@@ -27,7 +28,7 @@ class ClientCore(object):
         """
         self.log = get_logger(self)
         self.config = get_config()
-        self.system = SystemEvents(self)
+        self.system = ClientSystemEvents(self)
         self.crypto = Crypto()
         self._queue = {"_": queue.Queue()}
         self._proto = {}
@@ -74,9 +75,9 @@ class ClientCore(object):
         self._queue[channel].put_nowait(message)
 
 
-class SystemEvents(object):
+class ClientSystemEvents(object):
     """
-    Happens at system events.
+    Client system events
     """
     MASTER_PUBKEY_FILE = "master_public_key.pem"
     TOKEN_CIPHER_FILE = "master_token.bin"
@@ -90,46 +91,6 @@ class SystemEvents(object):
         if not os.path.exists(self.pki_path):
             self.log.info("creating directory for keys in: {}".format(self.pki_path))
             os.makedirs(self.pki_path)
-
-    def refresh_keys(self):
-        """
-        Refresh keys on the disk. If key do not exist, create new.
-
-        :return:
-        """
-        self.log.info("Refreshing keys")
-
-        for key, key_pem in zip(["private", "public"], self.core.crypto.create_rsa_keypair()):
-            key_path = os.path.join(self.pki_path, "{}.pem".format(key))
-            self.log.debug("Refreshing {} key as {}".format(key, key_path))
-            try:
-                os.remove(key_path)
-                self.log.debug("Key {} removed".format(key_path))
-            except OSError as ex:
-                if ex.errno != errno.ENOENT:
-                    self.log.error("Error removing {} key: {}".format(key, ex))
-            with open(key_path, "wb") as key_fh:
-                key_fh.write(key_pem)
-                self.log.debug("Key {} written".format(key_path))
-
-        self.log.info("Keys has been re-generated")
-
-    def check_keys(self) -> bool:
-        """
-        Check if keypair is there and master is registered.
-        :return:
-        """
-        self.log.info("Checking keys in PKI: {}".format(self.core.config.config_path))
-        incomplete_keys = 0
-        for key in ["public", "private"]:
-            if not os.path.exists(os.path.join(self.pki_path, "{}.pem".format(key))):
-                self.log.warning("{} key not found".format(key))
-                incomplete_keys += 1
-        if incomplete_keys:
-            self.log.error("Private/public key pair is incomplete or does not exist. Updating.")
-            self.refresh_keys()
-
-        return not bool(incomplete_keys)
 
     def check_master_pubkey(self) -> bool:
         """
