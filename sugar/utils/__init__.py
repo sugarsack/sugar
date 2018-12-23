c6b658f3f22a2683e24892106d3eaf5a6e4985d8
Bo Maryniuk <bo@suse.de>
2018-12-23 19:02:52 +0100

    Add token implementation for master local service connection, to prevent other users connect to the master

--- sugar/utils/__init__.py
+++ sugar/utils/__init__.py
@@ -1,22 +1,51 @@
-import logging
-import sys
+"""
+General runtime utilities.
+"""
+from __future__ import absolute_import, unicode_literals
+
+import tempfile
+import hashlib
+import os
+
+import sugar.utils.stringutils
+from sugar.lib.logger.manager import get_logger
+from sugar.utils.objects import Singleton
+
+log = get_logger(__name__)
 
 
-def setup_logger(current_level):
+@Singleton
+class MasterLocalToken(object):
     """
-    Setup logger to the app.
-
-    :return:
+    Master token for local connections.
     """
-    level = LOG_LEVELS.get(current_level, LOG_LEVELS['info'])
-    root = logging.getLogger()
-    root.setLevel(level)
 
-    handlers = []
-    handlers.append(logging.StreamHandler(sys.stdout))
+    def __init__(self):
+        """
+        Creates master local token.
 
-    for handler in handlers:
-        handler.setLevel(level)
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-        handler.setFormatter(formatter)
-        root.addHandler(handler)
+        :param path:
+        """
+        self._filename = tempfile.mktemp(".token", ".sugar.")
+        with os.fdopen(os.open(self._filename, os.O_WRONLY | os.O_CREAT, 0o600), "wb") as fh:
+            fh.write(sugar.utils.stringutils.to_bytes(hashlib.sha256(os.urandom(0xfff)).hexdigest()))
+
+    def get_token(self):
+        """
+        Get token from the disk.
+
+        :return:
+        """
+        with open(self._filename) as fh:
+            return fh.read().strip()
+
+    def cleanup(self):
+        """
+        Remove token file.
+
+        :return:
+        """
+        try:
+            os.remove(self._filename)
+        except Exception as ex:
+            log.error("General error while removing token file: {}".format(ex))
