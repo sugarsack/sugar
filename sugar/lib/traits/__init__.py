07e64547d34514761be48c98c7c73eb4fed2f714
Bo Maryniuk <bo@suse.de>
2018-12-23 15:55:22 +0100

    Add trait loader

--- sugar/lib/traits/__init__.py
+++ sugar/lib/traits/__init__.py
@@ -4,7 +4,9 @@ Sugar Traits
 from __future__ import absolute_import, unicode_literals
 
 import copy
+import collections
 from sugar.utils.objects import Singleton
+import sugar.lib.traits.features
 
 
 @Singleton
@@ -13,7 +15,21 @@ class Traits(object):
     Traits class
     """
     def __init__(self):
-        self._data = {}  # Traits container
+        self._data = collections.OrderedDict()
+        self.reload()
+
+    def reload(self):
+        """
+        [re]load traits.
+
+        :return:
+        """
+        self._data = {}
+        for t_obj_n in dir(sugar.lib.traits.features):
+            t_obj = getattr(sugar.lib.traits.features, t_obj_n)
+            f_provides, f_type = [getattr(t_obj, attr, None) for attr in ("_sugar_provides", "_sugar_type")]
+            if f_type == "trait" and f_provides is not None:
+                self._data[f_provides] = t_obj()
 
     @property
     def data(self):
