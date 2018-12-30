d03193d22406c59c03018e5aaedb86fddc242f80
Bo Maryniuk <bo@suse.de>
2018-12-30 18:16:06 +0100

    Add support for PEP8 empty lines between functions and methods.

---
+++ pylintplugins/pep8_emptylines.py
@@ -0,0 +1,88 @@
+"""
+PEP8: look for two empty lines between functions, one empty line between methods.
+"""
+
+import os
+
+from pylint import checkers
+from pylint import interfaces
+
+
+class PEP8EmptyLinesChecker(checkers.BaseChecker):
+    """
+    PEP8 asks for:
+      - Two empty lines between module functions
+      - One empty line between class methods
+    """
+    __implements__ = interfaces.IAstroidChecker
+
+    name = 'pep8-empty-lines'
+    msgs = {
+        'E3010': (
+            "Expected 1 blank line between class methods, 2 blank lines between functions.",
+            'pep8-empty-lines',
+            'Emitted when backslash is found at the end of the line, but no necessity of its use.'
+            ),
+        }
+
+    def get_empty_lines(self, offset, index):
+        """
+        Get empty lines from the offset.
+
+        :param offset:
+        :param index:
+        :return:
+        """
+        lines = 0
+        for char in index[:offset][::-1]:
+            if char == "c":
+                continue
+
+            if char == "-":
+                lines += 1
+            elif char == "d":
+                lines = None
+                break
+            else:
+                break
+        return lines
+
+    def visit_module(self, node):
+        """
+        Calculate lines.
+
+        :param node:
+        :return:
+        """
+        index = []
+        # Gather map of the source
+        with open(node.file) as mod_fh:
+            for idx, line in enumerate(mod_fh.read().split(os.linesep)):
+                if not line:
+                    index.append("-")
+                elif line.startswith("def "):
+                    index.append("f")
+                elif not line.startswith("def ") and line.strip().startswith("def "):
+                    index.append("m")
+                elif "'''" in line or '"""' in line or line.endswith("'") or line.endswith('"'):
+                    index.append("d")
+                elif line.strip().startswith("@"):
+                    index.append("c")
+                else:
+                    index.append("#")
+
+        for idx, element in enumerate(index):
+            if idx < 2:
+                continue
+            if element in ["f", "m"]:
+                offset = self.get_empty_lines(idx, index)
+                if offset is not None:
+                    if (element == "f" and offset != 2) or (element == "m" and offset != 1):
+                        self.add_message("pep8-empty-lines", node=node, line=idx + 1)
+
+
+def register(linter):
+    """
+    Required method to auto register this checker
+    """
+    linter.register_checker(PEP8EmptyLinesChecker(linter))
