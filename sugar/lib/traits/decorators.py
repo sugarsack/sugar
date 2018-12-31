573c97c8138804a96f88e5fd142b47eb50f61cd0
Bo Maryniuk <bo@suse.de>
2018-12-31 11:24:38 +0100

    Disable R8001, W0212 lints. Add docstrings to the inner functions.

--- sugar/lib/traits/decorators.py
+++ sugar/lib/traits/decorators.py
@@ -10,10 +10,28 @@ def trait(name):
 
     :return:
     """
-    def dec(f):
+    # pylint: disable=R8001,W0212
+    def dec(func):
+        """
+        Decorator function.
+
+        :param func:
+        :return:
+        """
         def attrset(*args, **kwargs):
-            return f(*args, **kwargs)
+            """
+            Container for the attributes of the function.
+
+            :param args:
+            :param kwargs:
+            :return:
+            """
+            return func(*args, **kwargs)
+
         attrset._sugar_provides = name
         attrset._sugar_type = "trait"
+
         return attrset
+
     return dec
+    # pylint: enable=R8001,W0212
