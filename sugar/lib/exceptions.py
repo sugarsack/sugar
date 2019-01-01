07525c23eda47d8cb8e0a897d9078c22fa2792ae
Bo Maryniuk <bo@suse.de>
2019-01-01 20:11:47 +0100

    Lintfix: multiple spaces after ":"

--- sugar/lib/exceptions.py
+++ sugar/lib/exceptions.py
@@ -30,8 +30,10 @@ class SugarException(Exception):
 
         :return:
         """
-        return {'message':  six.text_type(self) if six.PY3 else self.__unicode__(),
-                'args': self.args}
+        return {
+            'message':  six.text_type(self) if six.PY3 else self.__unicode__(),
+            'args': self.args
+        }
 
 
 # Runtime
