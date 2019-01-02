5060f75930a35a078b074cef5bfa42e1660e09e6
Bo Maryniuk <bo@suse.de>
2019-01-02 18:52:47 +0100

    Update header

--- sugar/__init__.py
+++ sugar/__init__.py
@@ -3,9 +3,8 @@ Sugar project.
 """
 
 __author__ = "Bo Maryniuk"
-__copyright__ = "Copyright 2018, Sugar Project"
-__credits__ = []
+__copyright__ = "Copyright 2018-2019, Sugar Project"
 __license__ = "Apache 2.0"
 __version__ = "0.0.1"
-__email__ = "bo@maryniuk.net"
+__maintainer__ = "Bo Maryniuk"
 __status__ = "Damn Bloody Alpha"
