c983a8e1825f0d5b520a6ac070326f8187dbda55
Bo Maryniuk <bo@suse.de>
2019-01-11 21:56:36 +0100

    Add gettext main scaffold

---
+++ sugar/lib/i18n/__init__.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+"""
+i18n, based on Zope 2 ideas.
+"""
+from __future__ import absolute_import, unicode_literals
+import os
+import locale
+
+
+class Gettext(object):
+    """
+    Get/add text
+    """
+
+    def __init__(self, domain="default", **conf):
+        """
+        Constructor.
+
+        :param domain: domain of the messages. Default: "default".
+        """
+        self.path = os.path.join(os.path.dirname(__file__), "locales", locale.getlocale()[0], domain, "messages.yaml")
+        self._plural_none = conf.get("none", 0)
+        self.few = conf.get("few", 3)
+
+    def load(self):
+        """
+        Load messages for the specific locale.
+
+        :return: None
+        """
+
+    def gettext(self, text, count=0):
+        """
+        Get or add text.
+
+        :param text: Text ID.
+        :param count: count for plurals
+        :return: translated text, if any
+        """
+        plural = "none"
+        if count == 1:
+            plural = "one"
+        elif 1 < count <= self.few:
+            plural = "few"
+        elif count > self.few + 1:
+            plural = "many"
+        return self.__get(text, plural=plural)
+
+    def __add(self, text, plural):
+        """
+        Adds a text to the messages and returns it back as is,
+        because the text is new.
+
+        :param text: Text ID.
+        :param plural: plurals section.
+        :return: translated text
+        """
+
+    def __get(self, text, plural):
+        """
+        Gets a text from the messages. If none found, adds one.
+
+        :param text: Text ID.
+        :param plural: plurals section.
+        :return: translated text.
+        """
+        # get stuff from the text
+        # if not, add to it and save.
+        # reload.
