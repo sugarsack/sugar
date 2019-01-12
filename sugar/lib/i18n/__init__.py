cd0be3ee73b37980c35573059df1e67aa329a86a
Bo Maryniuk <bo@suse.de>
2019-01-12 12:41:48 +0100

    Redesigned messages format and saving handling

--- sugar/lib/i18n/__init__.py
+++ sugar/lib/i18n/__init__.py
@@ -8,32 +8,29 @@ import yaml
 import locale
 import sugar.utils.files
 from sugar.lib.logger.manager import get_logger
+from sugar.utils.objects import Singleton
 
 
+@Singleton
 class GetText(object):
     """
     Get/add text
     """
 
-    def __init__(self, domain="default", **conf):
-        """
-        Constructor.
-
-        :param domain: domain of the messages. Default: "default".
-        """
+    def __init__(self):
         self.log = get_logger(self)
         for loc_val in [locale.getlocale()[0], "en_US"]:
-            self.path = os.path.join(os.path.dirname(__file__), "locales", loc_val, domain, "messages.yaml")
+            self.path = os.path.join(os.path.dirname(__file__), "locales", loc_val, "messages.yaml")
             if os.path.exists(self.path):
                 break
             else:
-                self.path = None
                 self.log.error("i18n messages file at {fname} is missing for locale {loc}.",
                                fname=self.path, loc=loc_val)
-        self._plural_none = conf.get("none", 0)
-        self.few = conf.get("few", 3)
-        self.load()
+                self.path = None
+        self._plural_none = 0
+        self.few = 3
         self.__translations = {}
+        self.load()
 
     def load(self):
         """
@@ -52,8 +49,11 @@ class GetText(object):
         :return: None
         """
         if self.path is not None:
-            with sugar.utils.files.fopen(self.path, "w") as tr_fh:
-                tr_fh.write(yaml.dump(self.__translations, default_flow_style=False))
+            if os.access(self.path, os.W_OK):
+                with sugar.utils.files.fopen(self.path, "w") as tr_fh:
+                    tr_fh.write(yaml.dump(self.__translations, default_flow_style=False, allow_unicode=True))
+            else:
+                self.log.error("Unable to update i18n messages at {}", self.path)
 
     def gettext(self, text, count=0):
         """
@@ -77,32 +77,42 @@ class GetText(object):
         Adds a text to the messages and returns it back as is,
         because the text is new.
         One entry structure is following:
-           {"sometext": ["sometranslation", {"many": "translations"}]}
-           {"sometext": ["sometranslation, {}]}
+           {"sometext": {"many": "translations"}}
+           {"sometext": {}}
 
         :param text: Text ID.
         :param plural: plurals section.
         :raises
         :return: None
         """
-        self.__translations.setdefault(text, [text, {plural: text}])  # Add the same text for further edit
+        translation = {plural: text}
+        if plural != "none":
+            translation[plural] = text
+        self.__translations.setdefault(text, translation)
+        self.save()
+
         return text
 
     def __get(self, text, plural):
         """
         Gets a text from the messages. If none found, adds one.
         One entry structure is following:
-           {"sometext": ["sometranslation", {"many": "translations"}]}
-           {"sometext": ["sometranslation, {}]}
+           {"sometext": {"many": "translations"}}
+           {"sometext": {}}
 
         :param text: Text ID.
         :param plural: plurals section.
         :return: translated text.
         """
-        tr_text, plurals = self.__translations.get(text, (None, None))
-        if tr_text is not None:
-            tr_text = plural.get(plural, tr_text)
+        translation = self.__translations.get(text, {})
+        if translation:
+            tr_text = translation.get(plural)
+            if not tr_text:
+                tr_text = self.__add(text, plural=plural)
         else:
             tr_text = self.__add(text, plural=plural)
 
         return tr_text
+
+
+gettext = GetText().gettext
