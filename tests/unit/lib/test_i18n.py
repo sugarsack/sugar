b1b18278a14ea0e98dc66018181a3742577a1e87
Bo Maryniuk <bo@suse.de>
2019-01-12 19:53:13 +0100

    Add UT for auto-add data

--- tests/unit/lib/test_i18n.py
+++ tests/unit/lib/test_i18n.py
@@ -43,3 +43,26 @@ apple:
         assert gtx.gettext("apple", 1) == "one apple"
         assert gtx.gettext("apple", 2) == "few apples"
         assert gtx.gettext("apple", 4) == "lots of apples"
+
+    @patch("os.path.join", MagicMock(return_value="/in/the/middle/of/nowhere"))
+    @patch("os.path.exists", MagicMock(return_value=True))
+    @patch("os.access", MagicMock(return_value=True))
+    def test_autoadd_data(self, gettext_class):
+        """
+        Test auto-add message to the translation.
+
+        :return: None
+        """
+        msg = "Homer Simpson"
+        yaml_mock = MagicMock()
+        with patch("sugar.utils.files.fopen", mock_open(read_data=""),
+                   create=True) as fhm, patch("yaml.dump", yaml_mock) as yml:
+            gtx = gettext_class()
+            for count in [0, 1, 3, 4]:
+                gtx.gettext(msg, count=count)
+
+            assert yaml_mock.called
+            translation_entry = yaml_mock.call_args_list[0][0][0]
+            assert msg in translation_entry
+            plurals = translation_entry[msg]
+            assert plurals["none"] == plurals["one"] == plurals["few"] == plurals["many"] == msg
