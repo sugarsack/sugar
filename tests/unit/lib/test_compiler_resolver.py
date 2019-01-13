9b8b57d972f418df501fca37f1f771ae1ceab76a
Bo Maryniuk <bo@suse.de>
2019-01-13 18:36:19 +0100

    Add UT for resolve path environments with unsanitised input

--- tests/unit/lib/test_compiler_resolver.py
+++ tests/unit/lib/test_compiler_resolver.py
@@ -2,6 +2,9 @@
 """
 Unit tests for compiler resolver.
 """
+from sugar.lib.compiler.objresolv import ObjectResolver
+from mock import MagicMock, patch
+import os
 
 
 class TestCompilerResolver:
@@ -9,9 +12,21 @@ class TestCompilerResolver:
     Unit tests for compiler resolver.
     """
 
-    def test_resolve_main(self):
+    @patch("os.path.exists", MagicMock(return_value=True))
+    @patch("os.makedirs", MagicMock())
+    def test_path_environments(self):
         """
-        Resolve main.
+        Resolve environment paths.
 
         :return: None
         """
+        pth = "/opt/sugar"
+
+        resolver = ObjectResolver(pth, "uat")
+        assert resolver.path == os.path.join(pth, "uat")
+
+        resolver = ObjectResolver(pth, "../../../../../../../etc")
+        assert resolver.path == os.path.join(pth, "etc")
+
+        for env in ["\\\\\\\\\\\\\\\\\\\\\n", "\n\t  \n", None, "main", "../../../../../../../"]:
+            assert ObjectResolver(pth, env).path == os.path.join(pth, "main")
