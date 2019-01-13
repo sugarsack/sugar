fd4b97ef8b47b2856e4cc70c50493718764244fa
Bo Maryniuk <bo@suse.de>
2019-01-13 17:52:39 +0100

    Fix path sanitiser for safe join

--- sugar/utils/sanitisers.py
+++ sugar/utils/sanitisers.py
@@ -5,11 +5,12 @@ Input and data sanitisers
 
 from __future__ import absolute_import, print_function, unicode_literals
 import re
+import os
 
 
-def join_path(*elements):
+def join_path(*elements, relative=False):
     """
-    Join path, cleaning-up all the elements
+    Join path safely, cleaning-up all the elements
 
     :param elements:
     :return: path string
@@ -17,6 +18,8 @@ def join_path(*elements):
     char = re.compile(r'[^A-Za-z0-9]')
     out = ['']
     for elm in elements:
-        out.append(char.sub('', elm))
+        for part in elm.lstrip(os.path.sep).split(os.path.sep):
+            out.append(char.sub('', part))
 
-    return '/'.join(out)
+    out = os.path.sep.join(out)
+    return out.lstrip(os.path.sep) if relative else out
