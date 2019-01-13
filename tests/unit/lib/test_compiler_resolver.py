d554777a6925b1fa2564ef063af750ce877528ae
Bo Maryniuk <bo@suse.de>
2019-01-13 18:11:03 +0100

    Add UT scaffold for the compiler resolver tests

---
+++ tests/unit/lib/test_compiler_resolver.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+"""
+Unit tests for compiler resolver.
+"""
+
+
+class TestCompilerResolver:
+    """
+    Unit tests for compiler resolver.
+    """
+
+    def test_resolve_main(self):
+        """
+        Resolve main.
+
+        :return: None
+        """
