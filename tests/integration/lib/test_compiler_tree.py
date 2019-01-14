bb3db03301f757515a94e1d13e3d4d988c09a9eb
Bo Maryniuk <bo@suse.de>
2019-01-14 18:53:51 +0100

    Implement integration tests for loading main state, explicit loading and uri loading

--- tests/integration/lib/test_compiler_tree.py
+++ tests/integration/lib/test_compiler_tree.py
@@ -3,26 +3,53 @@
 Unit tests for compiler tree.
 """
 
+import os
+import pytest
+
 from sugar.lib.compiler.objtree import ObjectTree
+from sugar.lib.compiler.objresolv import ObjectResolver
+import tests.integration
+from sugar.lib.outputters.console import MappingOutput
+
+
+@pytest.fixture
+def get_states_root():
+    """
+    Get states root (default env).
+
+    :return: path to the states root.
+    """
+    return os.path.join(os.path.dirname(tests.integration.__file__), "root", "states")
 
 
 class TestCompilerTree:
     """
     Tree compiler test case.
     """
-    def test_load_main_state(self):
+    def test_load_main_state(self, get_states_root):
         """
         Test loading main state (main.st) implicitly.
 
         :return: None
         """
+        otree = ObjectTree(ObjectResolver(get_states_root))
+        otree.load()
 
-    def test_load_main_state_explicitly(self):
+        assert list(otree.tree.keys()) == ['pyssl', 'ssh_configuration', 'httpd_installed']
+        assert otree.tree["ssh_configuration"]["pkg.installed"]["pkgs"][0] == "openssh-server"
+
+    def test_load_main_state_explicitly(self, get_states_root):
         """
         Test loading main state (main.st) explicitly.
 
         :return: None
         """
+
+        otree = ObjectTree(ObjectResolver(get_states_root))
+        otree.load("main")
+
+        assert list(otree.tree.keys()) == ['pyssl', 'ssh_configuration', 'httpd_installed']
+        assert otree.tree["ssh_configuration"]["pkg.installed"]["pkgs"][0] == "openssh-server"
 
     def test_load_state(self):
         """
@@ -38,9 +65,14 @@ class TestCompilerTree:
         :return: None
         """
 
-    def test_load_subset_by_uri(self):
+    def test_load_subset_by_uri(self, get_states_root):
         """
         Test loading templated state by uri subset.
 
         :return:
         """
+        otree = ObjectTree(ObjectResolver(get_states_root))
+        otree.load("services.ssl")
+        assert len(otree.tree) == 1
+        assert "pyssl" in otree.tree
+        assert otree.tree["pyssl"]["file"][0]["managed"]["name"] == "/etc/ssl.conf"
