11c67006695f3d072912dd25205157426378a06a
Bo Maryniuk <bo@suse.de>
2019-01-14 16:48:10 +0100

    Add UT scaffold tests for compiler tree

---
+++ tests/unit/lib/test_compiler_tree.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+"""
+Unit tests for compiler tree.
+"""
+
+from sugar.lib.compiler.objtree import ObjectTree
+
+
+class TestCompilerTree:
+    """
+    Tree compiler test case.
+    """
+    def test_load_main_state(self):
+        """
+        Test loading main state (main.st) implicitly.
+
+        :return: None
+        """
+
+    def test_load_main_state_explicitly(self):
+        """
+        Test loading main state (main.st) explicitly.
+
+        :return: None
+        """
+
+    def test_load_state(self):
+        """
+        Test loading state.
+
+        :return: None
+        """
+
+    def test_load_templated_state(self):
+        """
+        Test loading templated state.
+
+        :return: None
+        """
+
+    def test_load_subset_by_uri(self):
+        """
+        Test loading templated state by uri subset.
+
+        :return:
+        """
