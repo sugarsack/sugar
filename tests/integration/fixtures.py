aa90e4f7f4d20b51655a10045f1db2402b30f837
Bo Maryniuk <bo@suse.de>
2019-01-19 16:41:48 +0100

    Split states data paths

--- tests/integration/fixtures.py
+++ tests/integration/fixtures.py
@@ -6,12 +6,23 @@ import os
 import pytest
 import tests.integration
 
+COMMON_INTEGRATION_STATES_ROOT = os.path.join(os.path.dirname(tests.integration.__file__), "root")
+
 
 @pytest.fixture
 def get_barestates_root():
     """
-    Get states root (default env).
+    Get bare data fake states root (default env).
 
     :return: path to the states root.
     """
-    return os.path.join(os.path.dirname(tests.integration.__file__), "root", "barestates")
+    return os.path.join(COMMON_INTEGRATION_STATES_ROOT, "barestates")
+
+@pytest.fixture
+def get_states_root():
+    """
+    Get real states root (default env).
+
+    :return: path to the states root.
+    """
+    return os.path.join(COMMON_INTEGRATION_STATES_ROOT, "states")
