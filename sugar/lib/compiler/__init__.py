140b3223f8bbabee5d7db83c24305d81079afcbc
Bo Maryniuk <bo@suse.de>
2019-01-19 10:47:17 +0100

    Define State Compiler main API

--- sugar/lib/compiler/__init__.py
+++ sugar/lib/compiler/__init__.py
@@ -2,9 +2,36 @@
 """
 State Compiler.
 """
+import typing
+from sugar.lib.compiler.objresolv import ObjectResolver
+from sugar.lib.compiler.objtree import ObjectTree
+from sugar.lib.compiler.objtasks import StateTask
 
 
 class StateCompiler(object):
     """
     State Compiler class.
     """
+    def __init__(self, root: str, environment:str = ObjectResolver.DEFAULT_ENV):
+        self._object_resolver = ObjectTree(ObjectResolver(path=root, env=environment))
+
+    @property
+    def tree(self) -> dict:
+        """
+        Return object tree.
+
+        :return:
+        """
+        return self._object_resolver.tree
+
+    @property
+    def tasklist(self) -> typing.Tuple[StateTask]:
+        """
+        Return state tasks sequence.
+
+        Their concurrency should be decided
+        by the task performer code.
+
+        :return:
+        """
+        return (StateTask(), )
