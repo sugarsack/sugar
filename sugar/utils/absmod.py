c135e12f12c2a2ab08dafaf4b678a564e7c7bc3c
Bo Maryniuk <bo@suse.de>
2019-01-24 15:54:57 +0100

    Add base classes for state and runner modules

--- sugar/utils/absmod.py
+++ sugar/utils/absmod.py
@@ -4,16 +4,17 @@ Abstract module bases
 """
 from sugar.lib.traits import Traits
 from sugar.lib.loader import RunnerModuleLoader
+from sugar.transport import RunnerModulesMsgFactory, StateModulesMsgFactory
+
 import sugar.modules.runners
 
 
-class BaseStateModule:
+class BaseModule:
     """
-    Common class for state modules.
+    Base module
     """
     def __init__(self):
         self.__traits = Traits()
-        self.__modules = RunnerModuleLoader(sugar.modules.runners)  # Map should be sigleton, so no rescan happens
 
     @property
     def traits(self):
@@ -21,6 +22,40 @@ class BaseStateModule:
         Traits map.
         """
         return self.__traits
+
+    @staticmethod
+    def get_return():
+        raise NotImplementedError("Not implemented")
+
+
+class BaseRunnerModule(BaseModule):
+    """
+    Common class for runner modules.
+    """
+
+    @staticmethod
+    def get_return():
+        """
+        Create a return object instance
+        """
+        return RunnerModulesMsgFactory.create()
+
+
+class BaseStateModule(BaseModule):
+    """
+    Common class for state modules.
+    """
+
+    def __init__(self):
+        BaseModule.__init__(self)
+        self.__modules = RunnerModuleLoader(sugar.modules.runners)  # Map should be sigleton, so no rescan happens
+
+    @staticmethod
+    def get_return():
+        """
+        Create a return object instance
+        """
+        return StateModulesMsgFactory.create()
 
     @property
     def modules(self):
