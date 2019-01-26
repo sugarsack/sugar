2c553166049ed31f513304381218cf548c3416d0
Bo Maryniuk <bo@suse.de>
2019-01-26 17:47:49 +0100

    Add guard decorator

---
+++ sugar/lib/loader/util.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+"""
+Loader utilities: decorators etc.
+"""
+
+import sugar.utils.absmod
+import sugar.lib.exceptions
+
+
+def guard(func):
+    """
+    Guard call.
+
+    :param func: method or function to be
+    guarded by the result object.
+
+    :return: wrapper function
+    """
+    def caller(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+        except Exception as exc:
+            result = sugar.utils.absmod.ActionResult()
+            result.error = str(exc)
+            result.errcode = sugar.lib.exceptions.SugarException.get_errcode(exc)
+        return result
+
+    return caller
