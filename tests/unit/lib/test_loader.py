9fe7a1d8ea55b3fe0803c93cd0f3123325db6600
Bo Maryniuk <bo@suse.de>
2019-01-26 18:29:25 +0100

    Add a world-first module test :-)

---
+++ tests/unit/lib/test_loader.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+"""
+Lazy loader unit tests
+"""
+from sugar.lib.loader import SugarModuleLoader
+
+
+class TestSugarModuleLoader:
+    """
+    Sugar module loader test case.
+    """
+    def test_systemtest_ping(self):
+        """
+        Test system.test ping module.
+        :return:
+        """
+        sml = SugarModuleLoader()
+        out = sml.runners.system.test.ping()
+        assert isinstance(out, dict)
+        assert "text" in out
+        assert out["text"] == "pong"
+
+        out = sml.runners.system.test.ping(text="Hello, world!")
+        assert isinstance(out, dict)
+        assert "text" in out
+        assert out["text"] == "Hello, world!"
