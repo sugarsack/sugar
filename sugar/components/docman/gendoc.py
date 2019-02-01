4ae0950fc4cf3429a16a30644cbd7c8c340f4836
Bo Maryniuk <bo@suse.de>
2019-02-01 13:47:01 +0100

    Add documentation text generator

---
+++ sugar/components/docman/gendoc.py
@@ -0,0 +1,12 @@
+# config: utf-8
+"""
+Display or generate the documentation for the given module or function.
+"""
+
+
+class DocMaker:
+    """
+    Get a particular module or function
+    and create a documentation for it.
+    """
+    
