261ed3b2b45719fe9e822cca450360df1a73b385
Bo Maryniuk <bo@suse.de>
2019-02-03 11:57:10 +0100

    Move common stuff from the ModDoc to ModDocBase

---
+++ sugar/components/docman/docrnd.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+"""
+Document render.
+"""
+import os
+import abc
+import jinja2
+
+import sugar.utils.files
+from sugar.lib.compat import yaml
+
+
+class ModDocBase(abc.ABC):
+    """
+    Module documentation base class.
+    """
+    DOC = "doc.yaml"
+    EXAMPLES = "examples.yaml"
+    SCHEME = "scheme.yaml"
+
+    def __init__(self, mod_path, *functions):
+        """
+        Constructor.
+
+        :param mod_path: Module physical path
+        :param functions: List of functions to include (others will be removed).
+        """
+        self._mod_path = mod_path
+        self._functions = functions
+        self._docmap = {}
+
+        for section in [self.DOC, self.EXAMPLES, self.SCHEME]:
+            with sugar.utils.files.fopen(os.path.join(self._mod_path, section), 'r') as dfh:
+                self._docmap[section.split(".")[0]] = yaml.load(dfh.read())
+
+    def to_doc(self) -> str:
+        """
+        Documentation result.
+
+        :return: processible string
+        """
