77ae5cfffd470c5abfdef734bde1cfdce6db4443
Bo Maryniuk <bo@suse.de>
2019-02-14 22:05:47 +0100

    Start cdata storage (initial commit)

---
+++ sugar/components/server/cdata.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+"""
+Storage of the client arbitrary graph-like data
+and UCS algorithm implementation to search over it.
+"""
+
+
+class GraphMatch:
+    """
+    Graph data element and UCS search.
+    """
+
+
+class CDataStore:
+    """
+    Data storage/retriever in the directory.
+    """
+
+    def __init__(self, path):
+        self.path = path
