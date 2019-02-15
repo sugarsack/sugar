013202272c2d25493ac604bf0f7d44cadb461bd9
Bo Maryniuk <bo@suse.de>
2019-02-15 20:55:36 +0100

    Add UT for slicing nested dictionary with other objects

--- tests/unit/utils/test_structs.py
+++ tests/unit/utils/test_structs.py
@@ -12,6 +12,7 @@ class TestStructObjects:
     def test_path_slice_nested_dicts(self):
         """
         Test for slicing a dictionary by path.
+
         :return:
         """
         data = {
@@ -25,3 +26,19 @@ class TestStructObjects:
         }
         assert sugar.utils.structs.path_slice(data, "a", "b", "c", "d") == "bingo!"
 
+    def test_path_slice_nested_other_objects(self):
+        """
+        Test for slicing nested dictionary with arbitrary objects in it.
+
+        :return:
+        """
+        data = {
+            "a": {
+                "b": {
+                    "c": "bingo!",
+                },
+                "c": "wrong",
+            },
+            "b": [1, 2],
+        }
+        assert sugar.utils.structs.path_slice(data, "a", "b", "c") == "bingo!"
