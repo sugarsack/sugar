c6495206283f8b271ac3fca553fbd105130b563f
Bo Maryniuk <bo@suse.de>
2019-02-16 16:06:59 +0100

    Add InT for adding systems into the datastor

--- tests/integration/components/test_dstor.py
+++ tests/integration/components/test_dstor.py
@@ -2,7 +2,11 @@
 """
 Test data store runtime.
 """
-
+import os
+import errno
+import shutil
+import tempfile
+import pickle
 from sugar.components.server.cdatastore import CDataStore, CDataContainer
 
 
@@ -10,27 +14,45 @@ class TestDataStore:
     """
     Test suite for the datastore.
     """
+    store_path = None
+    store_ref = None
+
     @classmethod
     def setup_class(cls):
         """
-        Setup.
+        Setup test suite.
 
         :return:
         """
+        cls.store_path = tempfile.mkdtemp()
 
     @classmethod
     def teardown_class(cls):
         """
-        Teardown.
+        Teardown test suite.
 
         :return:
         """
+        shutil.rmtree(cls.store_path)
+
+    def _remove_inner_tree(self):
+        """
+        Remove inner tree.
+        :return:
+        """
+        try:
+            shutil.rmtree(os.path.join(self.store_path, "sugar"))
+        except OSError as exc:
+            if exc.errno != errno.ENOENT:
+                raise exc
 
     def setup_method(self, method):
         """
         Setup method
         :return:
         """
+        assert not os.path.exists(os.path.join(self.store_path, "sugar")), "Unclean setup"
+        self.store_ref = CDataStore(self.store_path)
 
     def teardown_method(self, method):
         """
@@ -39,6 +61,8 @@ class TestDataStore:
         :param method:
         :return:
         """
+        self.store_ref = None
+        self._remove_inner_tree()
 
     def test_add_system(self):
         """
@@ -46,6 +70,28 @@ class TestDataStore:
 
         :return:
         """
+        systems = [
+            ("807b8c1a8505c90781f6b4cc37e6cceb", "sugar.domain.org"),
+            ("ccd95d7d9247f00ded425c163f43d19a", "candy.domain.org"),
+            ("4008ebadf8fd65b33e775e3e98bfb9d7", "latte.domain.org"),
+        ]
+        for machine_id, hostname in systems:
+            container = CDataContainer(id=machine_id, host=hostname)
+            container.traits = {"os-family": "Linux", "machine-id": machine_id}
+            container.inherencies = {"hostname": hostname}
+            self.store_ref.add(container)
+
+        data_path = os.path.join(self.store_path, "sugar", "cdata")
+        for filename in os.listdir(data_path):
+            assert "." in filename
+            fname, ext = filename.split(".")
+            assert ext == "data"
+            assert fname in [mid[0] for mid in systems]
+
+            with open(os.path.join(data_path, filename), "rb") as dfh:
+                obj = pickle.load(dfh)
+                assert obj.id == fname
+                assert obj.traits["machine-id"] == obj.id == fname
 
     def test_delete_system(self):
         """
