00a802d5a49906edb5cce4c050f949fe502fcd17
Bo Maryniuk <bo@suse.de>
2019-02-16 15:15:43 +0100

    Add InT scaffold for data store.

---
+++ tests/integration/components/test_dstor.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+"""
+Test data store runtime.
+"""
+
+from sugar.components.server.cdatastore import CDataStore, CDataContainer
+
+
+class TestDataStore:
+    """
+    Test suite for the datastore.
+    """
+    @classmethod
+    def setup_class(cls):
+        """
+        Setup.
+
+        :return:
+        """
+
+    @classmethod
+    def teardown_class(cls):
+        """
+        Teardown.
+
+        :return:
+        """
+
+    def setup_method(self, method):
+        """
+        Setup method
+        :return:
+        """
+
+    def teardown_method(self, method):
+        """
+        Teardown method.
+
+        :param method:
+        :return:
+        """
+
+    def test_add_system(self):
+        """
+        Test add system to the data store.
+
+        :return:
+        """
+
+    def test_delete_system(self):
+        """
+        Test delete system from the data store.
+
+        :return:
+        """
+
+    def test_flush_all(self):
+        """
+        Test flush the data store.
+
+        :return:
+        """
+
+    def test_basic_search(self):
+        """
+        Test basic search over the data store.
+        NOTE: this search won't go over the details
+        of the particular search corner-cases
+        as there are other tests for this matter.
+
+        :return:
+        """
