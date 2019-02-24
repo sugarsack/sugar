a492859c14264ae9b28c51a03187b05d19dccad0
Bo Maryniuk <bo@suse.de>
2019-02-24 20:59:09 +0100

    Add Queue factory

--- sugar/lib/perq/__init__.py
+++ sugar/lib/perq/__init__.py
@@ -4,3 +4,18 @@ Persistent Queue
 """
 
 from sugar.lib.perq.fsqueue import FSQueue  # noqa
+
+
+class QueueFactory:
+    """
+    Queue Factory
+    """
+    @staticmethod
+    def fs_queue(path) -> FSQueue:
+        """
+        Create FS queue object.
+
+        :param path: xlog storage
+        :return: FSQueue instance
+        """
+        return FSQueue(path=path)
