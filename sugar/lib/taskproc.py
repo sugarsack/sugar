5011f172170b1d0700e9314dd3619e32256fcaef
Bo Maryniuk <bo@suse.de>
2019-02-25 20:13:44 +0100

    Add task processor (init)

--- sugar/lib/taskproc.py
+++ sugar/lib/taskproc.py
@@ -2,3 +2,20 @@
 """
 Task processing daemon.
 """
+
+from sugar.lib.perq import QueueFactory
+from sugar.lib.logger.manager import get_logger
+
+
+class TaskProcessor:
+    """
+    Concurrent task processor.
+    """
+    XLOG_PATH = "/tmp/task-processor"
+
+    def __init__(self, loader):
+        self.t_counter = 0
+        self.log = get_logger(self)
+        self.loader = loader
+        self._queue = QueueFactory.fs_queue(self.XLOG_PATH).use_notify()
+        self._d_stop = False
