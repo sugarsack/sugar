5405dbc86fc8ddcd68b69bf89bfd1cc71f188453
Bo Maryniuk <bo@suse.de>
2019-03-22 18:06:44 +0100

    Implement more on JobStorage APIs

--- sugar/lib/jobstore/storage.py
+++ sugar/lib/jobstore/storage.py
@@ -6,8 +6,10 @@ import os
 import errno
 import typing
 from sugar.lib.compiler.objtask import StateTask
-from sugar.lib.jobstore.database import database
 from sugar.lib.jobstore.entities import Job, Task, Call
+from sugar.utils.db import database
+from sugar.utils.sanitisers import join_path
+from sugar.utils.jid import jidstore
 from pony import orm
 
 
@@ -20,16 +22,31 @@ class JobStorage:
         self._db_path = None
         self.init()
 
-    def register(self, query: str, clientslist: list, expr: str, tasklist: typing.Tuple[StateTask]) -> None:
+    def new(self, query: str, clientslist: list, expr: str) -> str:
         """
         Register a new job.
 
         :param query: Issued matcher expression during the job state or runner.
         :param clientslist: Result of the matcher query
         :param expr: Expression of the job: either it is the name of the state or function etc. I.e. what was called.
-        :param tasklist: list of parsed tasks
-        :return:
+        :return: jid (new job id)
         """
+        jid = jidstore.create()
+        with orm.db_session:
+            Job(jid=jid, query=query, expr=expr)
+        return jid
+
+    def add_tasks(self, jid, *tasks: StateTask) -> None:
+        """
+        Adds a completed task to the job.
+
+        :param jid: job ID
+        :param tasks: List of tasks that has been completed
+        :return: None
+        """
+        with orm.db_session:
+            for task in tasks:
+                Job.get(jid=jid).tasks.create(idn=task.idn)
 
     def get_by_jid(self, jid) -> Job:
         """
@@ -38,7 +55,10 @@ class JobStorage:
         :param jid: job id.
         :return: Job object.
         """
-        return None
+        with orm.db_session:
+            job = Job.get(jid=jid)
+            job.clone()
+        return job
 
     def get_later_then(self, dt) -> list:
         """
@@ -58,7 +78,7 @@ class JobStorage:
         """
         return None
 
-    def all(self) -> list:
+    def all(self, limit=25, offset=0) -> list:
         """
         Get all existing jobs.
 
@@ -96,14 +116,17 @@ class JobStorage:
                     pass
             self.init()
 
-    def init(self):
+    def init(self) -> None:
         """
         Initialise database.
 
-        :return:
+        :return: None
         """
+        self._db_path = join_path(self._config.cache.path, "/master/jobs.data")
+        os.makedirs(os.path.dirname(self._db_path), exist_ok=True)
         database.bind(provider="sqlite", filename=self._db_path, create_db=True)
         database.generate_mapping(create_tables=True)
+
     def close(self):
         """
         Close and detach database.
