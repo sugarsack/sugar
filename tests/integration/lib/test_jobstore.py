b10b0ed28a7d73eea4f23d5b2126095c571cba3e
Bo Maryniuk <bo@suse.de>
2019-03-22 18:10:54 +0100

    Add integration test for updating job with the rendered task

--- tests/integration/lib/test_jobstore.py
+++ tests/integration/lib/test_jobstore.py
@@ -5,6 +5,7 @@ Job store tests.
 import os
 import shutil
 import pytest
+from sugar.lib.compiler import StateCompiler
 from sugar.lib.jobstore import JobStorage
 from sugar.config import get_config
 from tests.integration.fixtures import get_barestates_root
@@ -46,9 +47,26 @@ class TestBasicJobStore:
         jid = self.store.new(query=query, clientslist=clientslist, expr=uri)
         assert self.store.get_by_jid(jid).jid == jid
 
-    def test_update_job(self):
+    def test_update_job(self, get_barestates_root):
         """
         Update job.
 
         :return:
         """
+        query = ":a"
+        clientslist = ["web.sugarsack.org", "docs.sugarsack.org"]
+        uri = "job_store.test_jobstore_register_job"
+
+        # Create task
+        jid = self.store.new(query=query, clientslist=clientslist, expr=uri)
+        assert self.store.get_by_jid(jid).jid == jid
+
+        # Client compiles it
+        state = StateCompiler(get_barestates_root).compile(uri)
+
+        # Client updates the server
+        self.store.add_tasks(jid, *state.tasklist)
+        job = self.store.get_by_jid(jid)
+
+        assert len(job.tasks) == 1
+        assert state.tasklist[0].idn == next(iter(job.tasks)).idn
