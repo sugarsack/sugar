1976b85204dc95826517fa81323a5c09989289fd
Bo Maryniuk <bo@suse.de>
2019-03-22 18:05:47 +0100

    Move database reference to the common db utils

--- sugar/lib/jobstore/database.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-"""
-Database
-"""
-
-from pony import orm
-
-database = orm.Database()
---
+++ sugar/utils/db.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+"""
+Database helpers
+"""
+from pony import orm
+from sugar.transport.serialisable import Serialisable
+
+
+database = orm.Database()
+
+
+class SerialisableEntity:
+    """
+    Serialisable entity utils.
+    """
+    def clone(self):
+        """
+        Clone itself into the serialisable object.
+
+        :return: Serialisable
+        """
+        export_obj = Serialisable()
+        for attr in self.__class__.__dict__:
+            if not attr.startswith('_') and not callable(self.__class__.__dict__[attr]):
+                obj = getattr(self, attr)
+                setattr(export_obj, attr,
+                        [item.clone() for item in obj] if obj.__class__.__module__.startswith("pony.orm") else obj)
+        return export_obj
