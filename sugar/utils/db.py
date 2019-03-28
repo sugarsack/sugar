6145b17f47ce89f5e37aa5d8d07b68965bb63cb6
Bo Maryniuk <bo@suse.de>
2019-03-28 21:50:25 +0100

    Dereference weakref

--- sugar/utils/db.py
+++ sugar/utils/db.py
@@ -23,6 +23,8 @@ class SerialisableEntity:
         for attr in self.__class__.__dict__:
             if not attr.startswith('_') and not callable(self.__class__.__dict__[attr]):
                 obj = getattr(self, attr)
+                if obj is not None:
+                    obj = obj.__repr__.__self__
                 setattr(export_obj, attr,
                         [item.clone() for item in obj] if obj.__class__.__module__.startswith("pony.orm") else obj)
         return export_obj
