0acbed3bff7c7c664d23d2e3ed7905f92c5a43bc
Bo Maryniuk <bo@suse.de>
2019-06-26 18:01:06 +0200

    Add spawned list output function

--- sugar/modules/runners/system/run/_impl/posix.py
+++ sugar/modules/runners/system/run/_impl/posix.py
@@ -3,8 +3,12 @@
 This is the description of your module.
 Please make a better one.
 """
+import os
 import typing
+import subprocess
 from sugar.modules.runners.system.run.interface import RunInterface
+from sugar.utils.absmod import ActionResult
+from sugar.utils.stringutils import to_str
 
 
 class RunModule(RunInterface):
@@ -15,16 +19,36 @@ class RunModule(RunInterface):
     # Add another unix here (lowercase). Ban Windows as it should be different implementation instead.
     __platform__ = ["linux"]
 
-    def spawn(self, command: str) -> typing.Tuple[str, str]:
+    def spawn(self, command: str, args: typing.Union[list, str]) -> ActionResult:
         """
-        Run an arbitrary command in a subprocess spawn.
+        Run an arbitrary command in a subprocess spawn via pipe.
 
         :param command: a command to run
+        :param args: arguments
         :returns: tuple of STDOUT and STDERR
         """
+        if isinstance(args, str):
+            args = list(filter(None, args.split(" ")))
 
         result = self.new_result()
-        result["stdout"] = ""
-        result["stderr"] = ""
+        stdout, stderr = subprocess.Popen([command] + args, stdout=subprocess.PIPE).communicate()
+        result["stdout"] = to_str(stdout or b"")
+        result["stderr"] = to_str(stderr or b"")
 
         return result
+
+    def spawn_list(self, command: str, args: typing.Union[list, str]) -> ActionResult:
+        """
+        Run an arbitrary command in a subprocess spawn via pipe where STDOUT and STDERR are lists.
+
+        :param command: a command to run
+        :param args: arguments
+        :returns: tuple of STDOUT and STDERR
+        """
+        result = self.spawn(command=command, args=args)
+
+        for section in ["stdout", "stderr"]:
+            result[section] = result[section].split(os.linesep)
+
+        return result
+
--- sugar/modules/runners/system/run/interface.py
+++ sugar/modules/runners/system/run/interface.py
@@ -6,7 +6,7 @@ Please change this docstring for a better one.
 import abc
 import typing
 import platform
-from sugar.utils.absmod import BaseRunnerModule
+from sugar.utils.absmod import BaseRunnerModule, ActionResult
 
 
 class RunInterface(abc.ABC, BaseRunnerModule):
@@ -25,10 +25,21 @@ class RunInterface(abc.ABC, BaseRunnerModule):
         assert platform.system().lower() in self.__platform__
 
     @abc.abstractmethod
-    def spawn(self, command: str) -> typing.Tuple[str, str]:
+    def spawn(self, command: str, args: list) -> ActionResult:
         """
         Run an arbitrary command in a subprocess spawn.
 
         :param command: a command to run
-        :returns: tuple of STDOUT and STDERR
+        :param args: list of parameters
+        :returns: stdout and stderr slots in action result
+        """
+
+    @abc.abstractmethod
+    def spawn_list(self, command: str, args: list) -> ActionResult:
+        """
+        Run an arbitrary command in a subprocess spawn, with stdout/stderr are lists.
+
+        :param command: a command to run
+        :param args: list of parameters
+        :returns: stdout and stderr slots in action result
         """
